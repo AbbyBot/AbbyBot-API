fc6b4fcd6c9c2831620ad76796354d7111b4df33
Marcos Reyes <astronautmarkus@gmail.com>
2024-10-13 03:03:11 -0300

    Created '/privileges-info' endpoint for get data about AbbyBot privileges.

--- AbbyBot-API/app.py
+++ AbbyBot-API/app.py
@@ -1002,6 +1002,40 @@ def change_logs_channel():
         return jsonify({"error": "No guild found with the provided guild_id", "status_code": 404}), 404
 
 
+@app.route('/privileges-info', methods=['GET'])
+def privileges_info():
+    conn = get_db_connection("AbbyBot_Rei")
+    cursor = conn.cursor(dictionary=True)
+    
+    # If it is a GET request, it returns the privilege information
+    if request.method == 'GET':
+
+        query = "SELECT * FROM privileges"
+        cursor.execute(query)
+        privileges_data = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        if privileges_data:
+            # Transform the result into a more readable format
+            privileges_list = []
+            for privilege in privileges_data:
+                privileges_list.append({
+                    "id": privilege["id"],
+                    "privilege_name": privilege["privilege_name"],
+                    "value": privilege["value"],
+                    "rol_meaning": privilege["rol_meaning"],
+                    "how_to_get": privilege["how_to_get"],
+                    "xp_multiplier": privilege["xp_multiplier"],
+                    "exclusive_access": privilege["exclusive_access"]
+                })
+
+            return jsonify({
+                "privileges": privileges_list
+            })
+        else:
+            return jsonify({"error": "No privileges information found"}), 404
 
 
 
