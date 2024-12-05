581c5c6e9e89fe17c2e54025a2ca88e7268d89cf
Marcos Reyes <astronautmarkus@gmail.com>
2024-12-05 02:10:59 -0300

    Added backend verifications in /add-wishlist.

--- AbbyBot-API/app/utils/wishlist_utils.py
+++ AbbyBot-API/app/utils/wishlist_utils.py
@@ -1,15 +1,34 @@
 from ..utils.db import get_db_connection
 
 def add_wishlist(name, email, discord_username, reason, how_learned):
+    # Normalize data before insert
+    name = name.capitalize()
+    email = email.lower()
+    discord_username = discord_username.lower()
+    reason = reason.capitalize()
+    how_learned = how_learned.capitalize()
+
+    # Database connection
     conn = get_db_connection("AbbyBot_Asuka")
     cursor = conn.cursor(dictionary=True)
-    
-    query = """
+
+    # Check if the discord_username already exists in the wishlist
+    check_query = """
+    SELECT * FROM wishlist WHERE discord_username = %s;
+    """
+    cursor.execute(check_query, (discord_username,))
+    if cursor.fetchone() is not None:
+        cursor.close()
+        conn.close()
+        return "This Discord username has already been added to the wishlist."
+
+    # Insert the new wishlist entry if the discord_username does not exist
+    insert_query = """
     INSERT INTO wishlist (name, email, discord_username, reason, how_learned)
     VALUES (%s, %s, %s, %s, %s);
     """
-    cursor.execute(query, (name, email, discord_username, reason, how_learned))
-    conn.commit()  # Make commit to save the changes
+    cursor.execute(insert_query, (name, email, discord_username, reason, how_learned))
+    conn.commit() 
 
     cursor.close()
     conn.close()
