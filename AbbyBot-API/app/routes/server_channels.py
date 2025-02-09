27b59326f72f2b5234346e0a42a3c8a4e27270ab
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-09 04:05:41 -0300

    feat: Add Flasgger documentation for server channels endpoint with parameter and response details

--- AbbyBot-API/app/routes/server_channels.py
+++ AbbyBot-API/app/routes/server_channels.py
@@ -1,10 +1,45 @@
 from flask import Blueprint, request, jsonify
+from flasgger import swag_from
 from ..utils.db import get_db_connection
 from ..utils.server_channels_utils import fetch_server_channels, NoChannelsFoundError
 
 server_channels_bp = Blueprint('server_channels', __name__)
 
 @server_channels_bp.route('/server-channels', methods=['GET'])
+@swag_from({
+    'tags': ['AbbyBot Servers'],
+    'parameters': [
+        {
+            'name': 'guild_id',
+            'in': 'query',
+            'type': 'string',
+            'required': True,
+            'description': 'The ID of the guild to fetch channels for'
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'A list of server channels',
+            'schema': {
+                'type': 'array',
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'guild_id': {'type': 'string'},
+                        'channel_id': {'type': 'string'},
+                        'channel_title': {'type': 'string'}
+                    }
+                }
+            }
+        },
+        400: {
+            'description': 'Missing or invalid guild_id parameter'
+        },
+        404: {
+            'description': 'No channels found for the given guild_id'
+        }
+    }
+})
 def get_server_dashboard():
     guild_id = request.args.get('guild_id')
 
