cc4bb00c3fc6fe1175d1ff390f0c10a088f0a4c2
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-09 04:18:49 -0300

    refactor: Update Flasgger documentation to use correct tag for server stats endpoint and remove unused privileges endpoint

--- AbbyBot-API/app/routes/abbybot_server_stats.py
+++ AbbyBot-API/app/routes/abbybot_server_stats.py
@@ -6,7 +6,7 @@ abbybot_server_stats_bp = Blueprint('abbybot_server_stats', __name__)
 
 @abbybot_server_stats_bp.route('/server_stats', methods=['GET'])
 @swag_from({
-    'tags': ['Server stats'],
+    'tags': ['AbbyBot Servers'],
     'responses': {
         200: {
             'description': 'A list of server stats',
@@ -25,28 +25,3 @@ abbybot_server_stats_bp = Blueprint('abbybot_server_stats', __name__)
 def get_server_stats():
     stats = get_all_server_stats()
     return jsonify(stats)
-
-@abbybot_server_stats_bp.route('/privileges', methods=['GET'])
-@swag_from({
-    'tags': ['Server stats'],
-    'responses': {
-        200: {
-            'description': 'A list of privileges',
-            'examples': {
-                'application/json': [
-                    {
-                        "exclusive_access": "TBA",
-                        "how_to_get": "Automatically assigned to every new user.",
-                        "privilege_name": "Normal User 🐱",
-                        "rol_meaning": "Users who have just joined the server. No specific advantages but they earn 1.0 XP.",
-                        "value": 1,
-                        "xp_multiplier": "1.00"
-                    }
-                ]
-            }
-        }
-    }
-})
-def get_privileges():
-    privileges = get_all_privileges()
-    return jsonify(privileges)
