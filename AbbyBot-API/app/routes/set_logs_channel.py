fbfae085d685bef118d872a21c8c0644554be3c9
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-09 04:22:54 -0300

    feat: Add Flasgger documentation for set logs channel endpoint with parameters and response examples

--- AbbyBot-API/app/routes/set_logs_channel.py
+++ AbbyBot-API/app/routes/set_logs_channel.py
@@ -1,9 +1,63 @@
 from flask import Blueprint, jsonify, request
+from flasgger import swag_from
 from ..utils.logs_channel_utils import set_logs_channel
 
 set_logs_channel_bp = Blueprint('set_logs_channel', __name__)
 
 @set_logs_channel_bp.route('/set-logs_channel', methods=['POST'])
+@swag_from({
+    'tags': ['AbbyBot Servers'],
+    'parameters': [
+        {
+            'name': 'body',
+            'in': 'body',
+            'required': True,
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'guild_id': {
+                        'type': 'string',
+                        'description': 'The ID of the guild'
+                    },
+                    'logs_channel': {
+                        'type': 'string',
+                        'description': 'The ID of the logs channel'
+                    }
+                },
+                'required': ['guild_id', 'logs_channel']
+            }
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': 'Logs channel updated successfully',
+            'examples': {
+                'application/json': {
+                    'success': 'Changed logs_channel for guild {guild_id}',
+                    'status_code': 200
+                }
+            }
+        },
+        '400': {
+            'description': 'Invalid value for logs_channel',
+            'examples': {
+                'application/json': {
+                    'error': 'Invalid value for logs_channel. It must be a numeric value.',
+                    'status_code': 400
+                }
+            }
+        },
+        '404': {
+            'description': 'No guild found with the provided guild_id',
+            'examples': {
+                'application/json': {
+                    'error': 'No guild found with the provided guild_id',
+                    'status_code': 404
+                }
+            }
+        }
+    }
+})
 def change_logs_channel():
 
     guild_id = request.json.get('guild_id')
