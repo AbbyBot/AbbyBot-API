01da665fc8ab9f99dd482eb39c3c699e87218460
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-09 00:22:41 -0400

    refactor: changed all html templates for an unique generic HTML error handler

--- AbbyBot-API/app/routes/view_handler.py
+++ AbbyBot-API/app/routes/view_handler.py
@@ -4,24 +4,28 @@ view_handler_bp = Blueprint('view_handler', __name__)
 
 @view_handler_bp.app_errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('error_handler.html', error_code=404, error_message="Page Not Found"), 404
 
 @view_handler_bp.app_errorhandler(400)
 def bad_request_error(error):
-    return render_template('400.html'), 400
+    return render_template('error_handler.html', error_code=400, error_message="Bad Request"), 400
 
 @view_handler_bp.app_errorhandler(403)
 def forbidden_error(error):
-    return render_template('403.html'), 403
+    return render_template('error_handler.html', error_code=403, error_message="Forbidden"), 403
 
 @view_handler_bp.app_errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('error_handler.html', error_code=500, error_message="Internal Server Error"), 500
 
 @view_handler_bp.app_errorhandler(405)
 def method_not_allowed_error(error):
-    return render_template('405.html'), 405
+    return render_template('error_handler.html', error_code=405, error_message="Method Not Allowed"), 405
 
 @view_handler_bp.app_errorhandler(415)
 def unsupported_media_type_error(error):
-    return render_template('415.html'), 415
+    return render_template('error_handler.html', error_code=415, error_message="Unsupported Media Type"), 415
+
+@view_handler_bp.app_errorhandler(418)
+def teapot_error(error):
+    return render_template('error_handler.html', error_code=418, error_message="I'm a teapot"), 418
